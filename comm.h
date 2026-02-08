b3e1c1c82b00bd097d6820cfeb37c5efbe3fd7d8
qioxso <163643441+qioxso@users.noreply.github.com>
2026-02-09 01:51:51 +0800

    Refactor operation codes and add Uprobe structures

--- comm.h
+++ comm.h
@@ -1,6 +1,8 @@
+// comm.h
 #ifndef COMM_H
 #define COMM_H
 
+// 根据编译环境自动选择头文件
 #ifdef __KERNEL__
     #include <linux/types.h>
 #else
@@ -8,13 +10,19 @@
     #include <sys/types.h>
 #endif
 
+// 操作码定义
 enum OPERATIONS {
-    OP_READ_MEM = 0x801,
-    OP_WRITE_MEM = 0x802,
-    OP_SET_SWBP = 0x805,     // 设置软件断点
-    OP_DEL_SWBP = 0x806      // 删除软件断点
+    OP_INIT_KEY    = 0x800,
+    OP_READ_MEM    = 0x801,
+    OP_WRITE_MEM   = 0x802,
+    OP_MODULE_BASE = 0x803,
+    OP_SET_API_ADDR= 0x804,
+    // 新增 Uprobe 功能
+    OP_ADD_UPROBE  = 0x805,
+    OP_DEL_UPROBE  = 0x806
 };
 
+// 内存拷贝结构体
 typedef struct _COPY_MEMORY {
     pid_t pid;
     uintptr_t addr;
@@ -22,10 +30,17 @@ typedef struct _COPY_MEMORY {
     size_t size;
 } COPY_MEMORY;
 
-typedef struct _SWBP_INFO {
+// 模块基址结构体
+typedef struct _MODULE_BASE {
     pid_t pid;
-    uintptr_t addr;
-    uint32_t orig_instruction; // 原始指令
-} SWBP_INFO;
+    char* name;
+    uintptr_t base;
+} MODULE_BASE;
+
+// 新增：断点配置结构体
+typedef struct _UPROBE_CONFIG {
+    pid_t pid;      // 目标进程PID
+    uintptr_t addr; // 目标虚拟地址
+} UPROBE_CONFIG;
 
 #endif // COMM_H
