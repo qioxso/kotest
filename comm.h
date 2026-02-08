8f7645974acd2ff4355b345805f62783bda97d19
qioxso <163643441+qioxso@users.noreply.github.com>
2026-02-08 23:54:33 +0800

    Refactor comm.h by removing MODULE_BASE and clarifying comments
    Removed unused MODULE_BASE struct and updated comments for clarity.

--- comm.h
+++ comm.h
@@ -1,4 +1,3 @@
-// comm.h
 #ifndef COMM_H
 #define COMM_H
 
@@ -10,12 +9,10 @@
 #endif
 
 enum OPERATIONS {
-    OP_INIT_KEY = 0x800,
     OP_READ_MEM = 0x801,
     OP_WRITE_MEM = 0x802,
-    OP_MODULE_BASE = 0x803,
-    OP_SET_SWBP = 0x805,     // 改名为 SET_SWBP (软件断点)
-    OP_DEL_SWBP = 0x806
+    OP_SET_SWBP = 0x805,     // 设置软件断点
+    OP_DEL_SWBP = 0x806      // 删除软件断点
 };
 
 typedef struct _COPY_MEMORY {
@@ -25,17 +22,10 @@ typedef struct _COPY_MEMORY {
     size_t size;
 } COPY_MEMORY;
 
-typedef struct _MODULE_BASE {
-    pid_t pid;
-    char* name;
-    uintptr_t base;
-} MODULE_BASE;
-
-// [修改] 增加 orig_instruction 字段
 typedef struct _SWBP_INFO {
     pid_t pid;
     uintptr_t addr;
-    uint32_t orig_instruction; // 保存原始的4字节指令
+    uint32_t orig_instruction; // 原始指令
 } SWBP_INFO;
 
 #endif // COMM_H
