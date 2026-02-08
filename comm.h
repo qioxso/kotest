d26847604caa5833fd1b81c557cdca39fbe1a9fd
qioxso <163643441+qioxso@users.noreply.github.com>
2026-02-08 23:14:50 +0800

    Rename HWBP to SWBP and update related structures
    Renamed hardware breakpoint operations to software breakpoint operations and added a new field for original instruction in the software breakpoint structure.

--- comm.h
+++ comm.h
@@ -1,3 +1,4 @@
+// comm.h
 #ifndef COMM_H
 #define COMM_H
 
@@ -8,18 +9,15 @@
     #include <sys/types.h>
 #endif
 
-// 操作码定义
 enum OPERATIONS {
     OP_INIT_KEY = 0x800,
     OP_READ_MEM = 0x801,
     OP_WRITE_MEM = 0x802,
     OP_MODULE_BASE = 0x803,
-    OP_SET_API_ADDR = 0x804, // (保留占位，不再使用)
-    OP_SET_HWBP = 0x805,     // 设置硬件断点
-    OP_DEL_HWBP = 0x806      // 删除硬件断点
+    OP_SET_SWBP = 0x805,     // 改名为 SET_SWBP (软件断点)
+    OP_DEL_SWBP = 0x806
 };
 
-// 内存拷贝结构体
 typedef struct _COPY_MEMORY {
     pid_t pid;
     uintptr_t addr;
@@ -27,19 +25,17 @@ typedef struct _COPY_MEMORY {
     size_t size;
 } COPY_MEMORY;
 
-// 模块基址结构体
 typedef struct _MODULE_BASE {
     pid_t pid;
     char* name;
     uintptr_t base;
 } MODULE_BASE;
 
-// 硬件断点配置结构
-typedef struct _HWBP_INFO {
-    pid_t pid;          // 目标进程 PID
-    uintptr_t addr;     // 断点地址
-    int enable;         // (未使用)
-    int type;           // (未使用，默认 EXECUTE)
-} HWBP_INFO;
+// [修改] 增加 orig_instruction 字段
+typedef struct _SWBP_INFO {
+    pid_t pid;
+    uintptr_t addr;
+    uint32_t orig_instruction; // 保存原始的4字节指令
+} SWBP_INFO;
 
 #endif // COMM_H
